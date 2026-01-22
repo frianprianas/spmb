183750ee2a5666f3afc23abf6e67efa817634180
frianprianas <freeyhan87@hotmail.com>
2026-01-22 12:26:53 +0700

    Support VPS Env variable names

--- server/models/db.js
+++ server/models/db.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PASS || process.env.DB_PASSWORD, // VPS uses DB_PASSWORD
     {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || 'postgres',
         logging: console.log,
     }
 );
