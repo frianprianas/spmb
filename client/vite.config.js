02a87b361fe0e955e73b0c5f84b426e924341c6e
frianprianas <freeyhan87@hotmail.com>
2026-01-22 23:59:51 +0700

    fix proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
         host: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
-                changeOrigin: true,
+                target: 'http://127.0.0.1:5000',
+                changeOrigin: false,
                 secure: false,
             }
         }
--- server/index.js
+++ server/index.js
@@ -25,6 +25,10 @@ const Message = require('./models/Message');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
